10f52bdac51da7aa5521e447315599f306db3e60
Rinku Kothiya <rinku786@gmail.com>
2018-07-26 15:51:42 +0530

    Replaced realm & KDC server name in krb5.conf

--- configure_kerberos_server.py
+++ configure_kerberos_server.py
@@ -5,6 +5,7 @@ import os
 import sys
 import platform
 import math
+import fileinput
 import os.path
 from os import path
 
@@ -30,11 +31,27 @@ def install_func(package):
 
 ### Main code starts here 
 
+if len(sys.argv) < 2:
+    print "Usage:", sys.argv[0], "  <Realm>     <Admin Server Name>"
+    exit(1)
+
+Realm=sys.argv[1]
+ServerName=sys.argv[2]
+ConfigFile="/etc/krb5.conf"
+
 # Check OS
 check_os()
 
 # Install kerberos server packages
 install_func(server_packages)
 
-
+if os.path.isfile(ConfigFile):
+	for line in fileinput.input(files=ConfigFile,inplace=True, backup='.bak'):
+	    if "default_realm" in line:
+		LS = line.split(' ')
+		print "default_realm = ", Realm 
+	    elif "admin_server" in line:
+		print "admin_server = ", ServerName
+	    else:
+		print line,
 
